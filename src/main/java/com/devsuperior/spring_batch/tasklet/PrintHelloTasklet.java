2bee7fc83ef99c4e07a3a12a67520dadfa1a3720
JaimeDevS <jaime_mendes91@yahoo.com.br>
2025-12-19 18:30:06 -0300

    Projeto refatorado

---
+++ src/main/java/com/devsuperior/spring_batch/tasklet/PrintHelloTasklet.java
@@ -0,0 +1,25 @@
+package com.devsuperior.spring_batch.tasklet;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.StepContribution;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.infrastructure.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@StepScope
+public class PrintHelloTasklet implements Tasklet {
+
+	@Value("${name}")
+	private String name;
+	
+	@Override
+	public @Nullable RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+		System.out.println("Olá " + name + "!");
+		return RepeatStatus.FINISHED;
+	}
+
+}
